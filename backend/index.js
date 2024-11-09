4cf338914bb66fb519dc75b5f7d629ae265c99e4
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-11-09 18:15:54 +0530

    for adding in the mongouri and env

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
+
+require("dotenv").config();
+
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
@@ -9,7 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://localhost:27017/code-editor");
+mongoose.connect(process.env.MONGO_URI);
 
 app.get("/",function(req,res){
   try {
