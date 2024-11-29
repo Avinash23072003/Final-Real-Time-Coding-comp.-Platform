eac6d5459c63230add1efca9d9d5cb773e5c8503
Avinash Chitare <chitareavinash6@gmail.com>
2024-11-29 22:06:45 +0530

    Your commit message

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect("mongodb://localhost:27017");
 
 app.get("/",function(req,res){
   try {
